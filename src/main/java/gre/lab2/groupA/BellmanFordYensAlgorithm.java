687524367bc810b7de77528aa89465478a5a5fa0
michaelstrefeler <mstrefeler@me.com>
2024-05-17 13:57:13 +0200

    Partial algorithm

--- src/main/java/gre/lab2/groupA/BellmanFordYensAlgorithm.java
+++ src/main/java/gre/lab2/groupA/BellmanFordYensAlgorithm.java
@@ -6,51 +6,63 @@ import gre.lab2.graph.WeightedDigraph;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.Queue;
 
 public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm {
-  private static final int INFINITY = Integer.MAX_VALUE;
-  @Override
-  public BFYResult compute(WeightedDigraph graph, int from) {
-    final int N = graph.getNVertices();
+    private static final int INFINITY = Integer.MAX_VALUE;
+    @Override
+    public BFYResult compute(WeightedDigraph graph, int from) {
+        final int N = graph.getNVertices();
 
-    int[] pred = new int[N];
-    Arrays.fill(pred, BFYResult.UNREACHABLE);
+        int[] pred = new int[N];
+        Arrays.fill(pred, BFYResult.UNREACHABLE);
 
-    int[] d = new int[N];
-    Arrays.fill(d, INFINITY);
+        int[] d = new int[N];
+        Arrays.fill(d, INFINITY);
 
-    d[from] = 0;
+        d[from] = 0;
 
-    int k = 0;
+        int k = 0;
 
-    final int sentinel = N;
+        final int sentinel = N;
 
-    var Q = new ArrayDeque<Integer>();
+        // File FIFO
+        var Q = new ArrayDeque<Integer>();
 
-    Q.push(from);
-    Q.push(sentinel);
+        Q.push(from);
+        Q.push(sentinel);
 
-    while(!Q.isEmpty()) {
-      int current = Q.removeFirst();;
-      if(current == sentinel) {
-        if(!Q.isEmpty()) {
-          k++;
-          if(k == N) {
-            // todo : circuit absorbant trouvé
-          }
-          else {
-            Q.push(sentinel);
-          }
+        while(!Q.isEmpty()) {
+            int current = Q.removeFirst();
+            if(current == sentinel) {
+                if(!Q.isEmpty()) {
+                    k++;
+                    if(k == N) {
+                        // todo : circuit absorbant trouvé
+                        //return new BFYResult.NegativeCycle();
+                        System.out.println("R contient un circuit absorbant accessible depuis s");
+                    }
+                    else {
+                        Q.push(sentinel);
+                    }
+                }
+            }
+            else {
+                var outgoingEdges = graph.getOutgoingEdges(current);
+                // todo : continuer algo
+                for(var outgoingEdge : outgoingEdges) {
+                    int successor = outgoingEdge.to();
+                    // λj > λi + cij
+                    if(d[successor] > d[current] + outgoingEdge.weight()){
+                        d[successor] = d[current] + outgoingEdge.weight();
+                        pred[successor] = current;
+                        // TODO ne pas utiliser contains
+                        if(!Q.contains(successor)) {
+                            Q.push(successor);
+                        }
+                    }
+                }
+            }
         }
-      }
-      else {
-        var outgoingEdges = graph.getOutgoingEdges(current);
-        // todo : continuer algo
-      }
+        return new BFYResult.ShortestPathTree(d, pred);
     }
-
-    return null;
-  }
 }
--- src/main/java/gre/lab2/groupA/Main.java
+++ src/main/java/gre/lab2/groupA/Main.java
@@ -1,14 +1,21 @@
 package gre.lab2.groupA;
 
+import gre.lab2.graph.WeightedDigraph;
+import gre.lab2.graph.WeightedDigraphReader;
+
 import java.io.IOException;
 
 public final class Main {
-  public static void main(String[] args) throws IOException {
-    // TODO
-    //  - Renommage du package ;
-    //  - Écrire le code dans le package de votre groupe et UNIQUEMENT celui-ci ;
-    //  - Documentation soignée comprenant :
-    //    - la javadoc, avec auteurs et description des implémentations ;
-    //    - des commentaires sur les différentes parties de vos algorithmes.
-  }
+    public static void main(String[] args) throws IOException {
+        // TODO
+        //  - Renommage du package ;
+        //  - Écrire le code dans le package de votre groupe et UNIQUEMENT celui-ci ;
+        //  - Documentation soignée comprenant :
+        //    - la javadoc, avec auteurs et description des implémentations ;
+        //    - des commentaires sur les différentes parties de vos algorithmes.
+        WeightedDigraph reseau1 = WeightedDigraphReader.fromFile("data/reseau1.txt");
+        WeightedDigraph reseau2 = WeightedDigraphReader.fromFile("data/reseau2.txt");
+        WeightedDigraph reseau3 = WeightedDigraphReader.fromFile("data/reseau3.txt");
+        WeightedDigraph reseau4 = WeightedDigraphReader.fromFile("data/reseau4.txt");
+    }
 }
