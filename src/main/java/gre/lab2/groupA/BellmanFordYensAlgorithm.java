19deffdb0c54300afd632abe38be6979af58532e
michaelstrefeler <mstrefeler@me.com>
2024-05-25 16:06:01 +0200

    Added comments but didn't fix the algorithm

--- src/main/java/gre/lab2/groupA/BellmanFordYensAlgorithm.java
+++ src/main/java/gre/lab2/groupA/BellmanFordYensAlgorithm.java
@@ -6,54 +6,79 @@ import gre.lab2.graph.WeightedDigraph;
 
 import java.util.*;
 
+/**
+ * Class used to perform the Yen's improvement of the Bellman-Ford algorithm
+ * @author Lucas Charbonnier
+ * @author Michael Strefeler
+ */
 public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm {
     private static final int INFINITY = Integer.MAX_VALUE;
 
     private LinkedList<Integer> queue;
+
+    /**
+     * Array of booleans used to check if a vertex is in the queue or not
+     */
     private boolean[] inQueue;
 
+    /**
+     * Add a vertex to the queue
+     * @param vertex vertex to add
+     */
     void addToQueue(Integer vertex) {
         queue.addLast(vertex);
         inQueue[vertex] = true;
     }
 
+    /**
+     * Add a vertex to the queue if it's not already in said queue
+     * @param vertex vertex to add
+     */
     void addToQueueIfNotAlreadyIn(Integer vertex) {
         if(!inQueue[vertex])
             addToQueue(vertex);
     }
 
+    /**
+     * Get the next vertex from the FIFO queue
+     * @return the number of that vertex
+     */
     Integer nextFromQueue() {
         var item = queue.removeFirst();
         inQueue[item] = false;
         return item;
     }
 
+    /**
+     *
+     * @param graph the graph that we want to perform the algorithm on
+     * @param from the vertex from where the algorithm needs to start (source)
+     * @return the result of the algorithm as an instance of the {@link BFYResult} class
+     */
     @Override
     public BFYResult compute(WeightedDigraph graph, int from) {
-        final int N = graph.getNVertices();
+        final int N = graph.getNVertices(); // Number of vertices
 
-        int[] pred = new int[N];
+        int[] pred = new int[N]; // Array of predecessors
         Arrays.fill(pred, BFYResult.UNREACHABLE);
 
-        int[] d = new int[N];
+        int[] d = new int[N]; // Array of distances from the source
         Arrays.fill(d, INFINITY);
 
         d[from] = 0;
 
-        int k = 0;
+        int k = 0; // Number of iterations
 
-        final int sentinel = N;
-
-        // File FIFO
+        // FIFO queue
         queue = new LinkedList<Integer>();
         inQueue = new boolean[N + 1]; // n + 1 for the sentinel
 
         addToQueue(from);
-        addToQueue(sentinel);
+        addToQueue(N);
 
         while(!queue.isEmpty()) {
             int current = nextFromQueue();
-            if(current == sentinel) {
+            if(current == N) {
                 if(!queue.isEmpty()) {
                     k++;
                     if(k == N) {
@@ -92,19 +117,18 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
                                 return new BFYResult.NegativeCycle(circuitInCorrectOrder, circuitLength);
                             }
                         }
-                        throw new RuntimeException("absorbing circuit should have been found");
+                        throw new RuntimeException("Un circuit absorbant aurait dû être trouvé!");
                     }
                     else {
-                        addToQueue(sentinel);
+                        addToQueue(N); // Add the sentinel back to the queue
                     }
                 }
             }
             else {
                 var outgoingEdges = graph.getOutgoingEdges(current);
-                // todo : continuer algo
                 for(var outgoingEdge : outgoingEdges) {
                     int successor = outgoingEdge.to();
-                    // λj > λi + cij
+                    // λj > λi + cij --> update distance and predecessor of the 'successor' vertex
                     if(d[successor] > d[current] + outgoingEdge.weight()){
                         d[successor] = d[current] + outgoingEdge.weight();
                         pred[successor] = current;
--- src/main/java/gre/lab2/groupA/Main.java
+++ src/main/java/gre/lab2/groupA/Main.java
@@ -7,14 +7,13 @@ import gre.lab2.graph.WeightedDigraphReader;
 import java.io.IOException;
 import java.util.Arrays;
 
+/**
+ * Class used to test our implementation the algorithm
+ * @author Lucas Charbonnier
+ * @author Michael Strefeler
+ */
 public final class Main {
     public static void main(String[] args) throws IOException {
-        // TODO
-        //  - Renommage du package ;
-        //  - Écrire le code dans le package de votre groupe et UNIQUEMENT celui-ci ;
-        //  - Documentation soignée comprenant :
-        //    - la javadoc, avec auteurs et description des implémentations ;
-        //    - des commentaires sur les différentes parties de vos algorithmes.
         WeightedDigraph reseau1 = WeightedDigraphReader.fromFile("data/reseau1.txt");
         WeightedDigraph reseau2 = WeightedDigraphReader.fromFile("data/reseau2.txt");
         WeightedDigraph reseau3 = WeightedDigraphReader.fromFile("data/reseau3.txt");
@@ -24,8 +23,6 @@ public final class Main {
         WeightedDigraph reseau_5_3 = WeightedDigraphReader.fromFile("data/reseau-ex-5_3.txt");
 
         var algorithm = new BellmanFordYensAlgorithm();
-
-
 
         var result_5_2 = algorithm.compute(reseau_5_2, 0);
         var result_5_3 = algorithm.compute(reseau_5_3, 0);
@@ -42,6 +39,11 @@ public final class Main {
         printBFYResult(result4, "Réseau 4");
     }
 
+    /**
+     * Print the result of our algorithm to comply with what was asked
+     * @param result result of the algorithm to print
+     * @param name name of the weighted digraph that will be printed
+     */
     static void printBFYResult(BFYResult result, String name) {
         String bar = "----------[ " + name + " ]----------";
         System.out.println(bar);
